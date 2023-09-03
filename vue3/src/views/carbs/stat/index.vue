111c368d450ddc9559c90959da5a7135a5027ea5
chzhm159 <chzhm159@gmail.com>
2023-09-03 19:22:26 +0800

    增加一个柱状图统计页面

--- vue3/src/views/carbs/stat/index.vue
+++ vue3/src/views/carbs/stat/index.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="p-4">
-    <a-form  :label-col="{xs: {span: 24}, sm: {span: 6}}" :wrapper-col="{xs: {span: 24}, sm: {span: 18}}" >
-            <a-row>
-                <a-col :span="8">
-                    <a-form-item label="日投喂量" name="title">
-                        <a-input placeholder="请输入 日投喂量"/>
-                    </a-form-item>
-                </a-col>
-                 <a-col :span="8">
-                    <a-form-item label="残饵量颗数" name="title">
-                        <a-input placeholder="请输入 残饵量颗数" />
-                    </a-form-item>
-                </a-col>
-                
-            </a-row>
-        </a-form>
-      </div>
-  <div class="p-4">
+   
+    <a-form ref="queryForm" :model="planModel" :label-col="labelCol" :wrapper-col="wrapperCol" >
+      <a-row :gutter="2">
+        <a-col :span="8">
+            <a-form-item label="选择时间范围">
+          <a-range-picker style="width:250px"  />
+          <a-button type="primary" preIcon="ant-design:search-outlined" @click="searchQuery">查询</a-button>
+        </a-form-item>
+        </a-col>
+        <a-col >
+            
+        </a-col>
+    </a-row>  
+  </a-form>
+  
+ <div class="p-4">
     <a-card :bordered="false" style="height: 100%">
-      <a-tabs v-model:activeKey="activeKey" animated @change="tabChange">
+      <a-tabs >
         <a-tab-pane key="bar" tab="摄食量可视化">
-        
           <Bar :chartData="dataSource" height="60vh"></Bar>
         </a-tab-pane>
         
@@ -33,18 +30,26 @@
   import { defHttp } from '/@/utils/http/axios';
   import { ref, unref, reactive } from 'vue';
   import Bar from '/@/components/chart/Bar.vue';
-
-  const activeKey = ref('bar');
-  const barType = ref('year');
-  const pieType = ref('year');
   const dataSource = ref([]);
-  const url = reactive({
-    getYearCountInfo: '/mock/api/report/getYearCountInfo',
-    getMonthCountInfo: '/mock/api/report/getMonthCountInfo',
-    getCntrNoCountInfo: '/mock/api/report/getCntrNoCountInfo',
-    getCabinetCountInfo: '/mock/api/report/getCabinetCountInfo',
-  });
-
+  const queryForm = ref();
+  const planModel = ref();
+  const labelCol = reactive({
+    xs: {span: 24},
+    sm: {span: 6},
+})
+ const wrapperCol = reactive({
+    xs: {span: 24},
+    sm: {span: 16},
+})
+    /**
+     * 表单提交事件
+     * */
+    async function handleSubmit() {
+        console.log("查询,handleSubmit");
+    }
+  async function searchQuery(){
+    console.log("查询");
+  }
   async function loadDate(url, type, params) {
     demoData();
     // const res = await defHttp.get({ url, params }, { isTransformResponse: false, errorMessageMode: 'none' });
@@ -84,61 +89,6 @@
       }
     ]
     dataSource.value=data;
-  }
-  function getYearCountSource(data) {
-    for (let i = 0; i < data.length; i++) {
-      dataSource.value.push({
-        name: `${data[i].year}年`,
-        value: data[i].yearcount,
-      });
-    }
-  }
-  function getMonthCountSource(data) {
-    for (let i = 0; i < data.length; i++) {
-      dataSource.value.push({
-        name: `${data[i].month}`,
-        value: data[i].monthcount,
-      });
-    }
-  }
-  function getCategoryCountSource(data) {
-    for (let i = 0; i < data.length; i++) {
-      dataSource.value.push({
-        name: `${data[i].classifyname}`,
-        value: data[i].cntrnocount,
-      });
-    }
-  }
-  function getCabinetCountSource(data) {
-    for (let i = 0; i < data.length; i++) {
-      dataSource.value.push({
-        name: `${data[i].cabinetname}`,
-        value: data[i].cabinetcocunt,
-      });
-    }
-  }
-  // 选择统计类别
-  function statisticst(e) {
-    if (unref(activeKey) === 'pie') {
-      loadDate(getUrl(unref(pieType)), unref(pieType), {});
-    } else {
-      loadDate(getUrl(unref(barType)), unref(barType), {});
-    }
-  }
-
-  function getUrl(type) {
-    if (type === 'year') {
-      return url.getYearCountInfo;
-    }
-    if (type === 'month') {
-      return url.getMonthCountInfo;
-    }
-    if (type === 'category') {
-      return url.getCntrNoCountInfo;
-    }
-    if (type === 'cabinet') {
-      return url.getCabinetCountInfo;
-    }
   }
   //tab切换
   function tabChange(key) {
