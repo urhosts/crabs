aa57c96c2c024cc85158286c39c193bd2c2a853a
hosts <18193316+urhosts@users.noreply.github.com>
2023-09-15 19:24:42 +0800

    Create StatResultBait.java
    创建类

---
+++ server/jeecg-module-crabs/src/main/java/org/jeecg/modules/crabs/bait/entity/StatResultBait.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.crabs.bait.entity;
+
+import lombok.Data;
+
+@Data
+public class StatResultBait {
+    private static final long serialVersionUID = 1L;
+
+    private String createTime;
+
+    private Integer baitCount;
+
+    private Integer baitInput;
+
+    private double baitIntake;
+
+}
