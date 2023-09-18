3585877f990860e20c14b174183eb1dc7e853d48
hosts <18193316+urhosts@users.noreply.github.com>
2023-09-18 10:52:02 +0800

    Update RemainingBait.data.ts
    918

--- vue3/src/views/carbs/bait/RemainingBait.data.ts
+++ vue3/src/views/carbs/bait/RemainingBait.data.ts
@@ -4,23 +4,28 @@ import { rules} from '/@/utils/helper/validator';
 import { render } from '/@/utils/common/renderUtils';
 //列表数据
 export const columns: BasicColumn[] = [
-   {
+  {
+    title: '创建时间',
+    align:"center",
+    dataIndex: 'createTime'
+  },
+  {
     title: '原始图片',
     align:"center",
     dataIndex: 'baitPic',
     customRender:render.renderImage,
-   },
-   {
+  },
+  {
     title: '识别结果',
     align:"center",
     dataIndex: 'recognitionPic',
     customRender:render.renderImage,
-   },
-   {
+  },
+  {
     title: '残饵计数',
     align:"center",
     dataIndex: 'baitCount'
-   },
+  },
   {
     title: '投喂量',
     align:"center",
@@ -35,24 +40,24 @@ export const formSchema: FormSchema[] = [
   {
     label: '原始图片',
     field: 'baitPic',
-     component: 'JImageUpload',
-     componentProps:{
+    component: 'JImageUpload',
+    componentProps:{
       fileMax : 1
-      },
+    },
     dynamicRules: ({model,schema}) => {
-          return [
-                 { required: true, message: '请输入原始图片!'},
-          ];
-     },
+      return [
+        { required: true, message: '请输入原始图片!'},
+      ];
+    },
   },
   {
     label: '识别结果',
     field: 'recognitionPic',
-     component: 'JImageUpload',
-     componentProps:{
+    component: 'JImageUpload',
+    componentProps:{
       text:"",
       listType:"picture-card"
-      },
+    },
     dynamicDisabled:true
   },
   {
@@ -66,21 +71,21 @@ export const formSchema: FormSchema[] = [
     field: 'baitInput',
     component: 'InputNumber',
   },
-	// TODO 主键隐藏字段，目前写死为ID
-	{
-	  label: '',
-	  field: 'id',
-	  component: 'Input',
-	  show: false
-	},
+  // TODO 主键隐藏字段，目前写死为ID
+  {
+    label: '',
+    field: 'id',
+    component: 'Input',
+    show: false
+  },
 ];
 
 
 
 /**
-* 流程表单调用这个方法获取formSchema
-* @param param
-*/
+ * 流程表单调用这个方法获取formSchema
+ * @param param
+ */
 export function getBpmFormSchema(_formData): FormSchema[]{
   // 默认和原始表单保持一致 如果流程中配置了权限数据，这里需要单独处理formSchema
   return formSchema;
