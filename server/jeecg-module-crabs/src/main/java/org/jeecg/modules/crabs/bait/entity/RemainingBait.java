7a9b9f82b350eecb0460b6318782ed8082fc773f
hosts <18193316+urhosts@users.noreply.github.com>
2023-10-09 08:41:46 +0800

    Update RemainingBait.java
    优化import

--- server/jeecg-module-crabs/src/main/java/org/jeecg/modules/crabs/bait/entity/RemainingBait.java
+++ server/jeecg-module-crabs/src/main/java/org/jeecg/modules/crabs/bait/entity/RemainingBait.java
@@ -1,22 +1,19 @@
 package org.jeecg.modules.crabs.bait.entity;
 
-import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.math.BigDecimal;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import org.jeecg.common.aspect.annotation.Dict;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Description: 残饵计数识别
