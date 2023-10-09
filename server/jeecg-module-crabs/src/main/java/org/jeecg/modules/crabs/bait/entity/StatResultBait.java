0edaefd10bebcd133e2aee157f3a7374e2e8127a
hosts <18193316+urhosts@users.noreply.github.com>
2023-10-09 10:27:20 +0800

    Update StatResultBait.java
    修正返回body

--- server/jeecg-module-crabs/src/main/java/org/jeecg/modules/crabs/bait/entity/StatResultBait.java
+++ server/jeecg-module-crabs/src/main/java/org/jeecg/modules/crabs/bait/entity/StatResultBait.java
@@ -6,13 +6,32 @@ import lombok.Data;
 public class StatResultBait {
     private static final long serialVersionUID = 1L;
 
+    // 创建时间
     private String createTime;
 
+    // 饵料个数
     private Integer baitCount;
 
+    // 投喂量
     private Integer baitInput;
 
+    // 摄食量
     private double baitIntake;
+
+    // 识别表溶解氧
+    private Double recoDissolvedOxygen;
+
+    // 预测表溶解氧
+    private Double antiDissolvedOxygen;
+
+    // 识别表螃蟹只数
+    private Integer recoCrabsCount;
+
+    // 预测表螃蟹只数
+    private Integer antiCrabsCount;
+
+    // 预测表明日投喂量
+    private Double baitPrediction;
 
     private String name;
 
